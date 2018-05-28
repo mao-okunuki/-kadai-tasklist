51f772fbad0c9b0a765f2b9425a2106865c32368
mao.okunuki <mao.okunuki@rakuten.com>
2018-05-28 05:31:43 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 
-<h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+<h1>id = {{ $task->id }} の タスク詳細ページ</h1>
 
-<p>{{ $task->content }}</p>
-    
- {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!} 
- 
- {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-    {!! Form::submit('削除') !!}
+    <p>タイトル: {{ $task->status }}</p>
+    <p>メッセージ: {{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
+
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
     {!! Form::close() !!}
 
 @endsection
